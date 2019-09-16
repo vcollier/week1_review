2199a26c181c0b144a471fdd86ad93892e309531
Collier <v_collier@ats-hr420-dm23.fc.ca>
2019-09-16 18:49:52 -0400

    adding more stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import Person from "./modules/DataObject.js";
+import People from "./modules/DataObject.js";
 
 // IIFE - immediately invoced function expression
 (() => {
@@ -6,12 +6,38 @@ import Person from "./modules/DataObject.js";
     // grab the bio container and get ready to populate its content
     const bioInfo = document.querySelector('.bio-wrapper').children;
 
-    // this refers ti the heading tag (the first child of the bio-wrapper div)
-    bioInfo[0].textContent = Person.name;
-    // this refers to the paragraph tag (the second child of the bio-wrapper div)
-    bioInfo[1].textContent = Person.role;
+    function showProfData() {
+        // change data on the page here
 
-    bioInfo[2].src = `images/${Person.pic}`
+        // this is the label on the button (the name)
+        let currentProf = this.textContent;
+
+        // this refers ti the heading tag (the first child of the bio-wrapper div)
+        bioInfo[0].textContent = People[currentProf].name;
+        // this refers to the paragraph tag (the second child of the bio-wrapper div)
+        bioInfo[1].textContent = People[currentProf].role;
+
+        bioInfo[2].src = `images/${People[currentProf].pic}`;
+    }
+
+    for (let prof in People) {
+        console.log(prof);
+
+        // creat a button for every prof (every entry) in our project
+        let teamButton = document.createElement('button');
+
+        //set the button's label (text) to the prof name (the current entry in the object)
+        teamButton.textContent = prof;
+
+        // dd some event handling so that when we click one of our new buttons, we can show the right data
+        teamButton.addEventListener('click', showProfData);
+
+        // add the button to the top section on our web page
+        document.querySelector("#section1").appendChild(teamButton);
+
+    }
+
+    
 
 
 })();
--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -2,12 +2,37 @@
 // remember to separate the keys and values with a comma
 // and that's about it!
 // and use a colon between the key and the value
-const Person = {
-    name: "Trevor",
-    eyeColor: "blue",
-    hairColor: "red",
-    role: "Professor",
-    pic: "olaf.jpeg"
+const Team = {
+    
+    Trevor: {
+        name: "Trevor",
+        eyeColor: "blue",
+        hairColor: "red",
+        role: "Professor",
+        pic: "olaf.jpeg"},
+
+    Joe: {
+        name: "Joe",
+        eyeColor: "brown",
+        hairColor: "greyish",
+        role: "Professor",
+        pic: "pooh.jpeg"},
+
+    Jarrod: {
+        name: "Jarrod",
+        eyeColor: "brown",
+        hairColor: "blonde",
+        role: "Professor",
+        pic: "zoolander.jpg"},
+
+    Justin: {
+        name: "Justin",
+        eyeColor: "brown",
+        hairColor: "kind of an evil black",
+        role: "Coordinator",
+        pic: "justin.jpeg"},
 }
 
-export default Person;
+
+// export {team, people};
+export default Team;
